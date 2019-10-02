2f523b8c73e2867f8ebcc9b0de7010abaca3f523
Ryan Showalter <ryanshowalter76@gmail.com>
2019-10-02 18:07:14 -0400

    Add permission checking for edits

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -23,6 +23,10 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Gate::define('update-post', function ($user, $post) {
+            return $user->id == $post->user_id;
+        });
+        
         $this->registerPolicies();
 
         //
